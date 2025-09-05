b0fbd6c953029c4a58501ab552f4d03c3746e935
nalll <nalll88@mail.ru>
2025-09-05 14:56:33 +0300

    1_commit: add func

---
+++ func/is_prime.py
@@ -0,0 +1,10 @@
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
---
+++ test/test_factorial.py
@@ -0,0 +1,4 @@
+from func.factorial import factorial
+
+def test_factorial():
+    assert factorial(0) == 1
